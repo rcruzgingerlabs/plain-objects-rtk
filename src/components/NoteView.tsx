eda6b70c5054d46d53d6df2285e14deeddc7d5e7
Ryan Cruz <rcruz@gingerlabs.com>
2024-11-03 00:42:41 -0700

    Update baseline

--- src/components/NoteView.tsx
+++ src/components/NoteView.tsx
@@ -1,17 +1,20 @@
 import { useAppSelector } from "@/app/hooks"
-import { selectActiveNote } from "@/features/notes/selectors"
+import { type RootState } from "@/app/store"
 import "./NoteView.css"
 
 export default function NoteView() {
-  const note = useAppSelector(selectActiveNote)
-
-  if (!note) {
+  const activeNoteId = useAppSelector((s: RootState) => s.notes.activeNoteId)!
+  const noteTitle = useAppSelector((s: RootState) =>
+    activeNoteId
+      ? s.notes.entities[activeNoteId].session?.getTitle()
+      : undefined,
+  )
+  if (!activeNoteId) {
     return null
   }
-
   return (
     <main className="noteview">
-      <div className="notetitle">{note.session?.getTitle()}</div>
+      <div className="notetitle">{noteTitle}</div>
     </main>
   )
 }
